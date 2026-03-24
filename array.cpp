08dad5795ee3834c451e203d2f872a3ab8e69cf9
thiru <sthiruselva11@gmail.com>
2026-03-24 13:04:59 +0530

    newlyadded

---
+++ array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+	int *arr;
+	arr=new int[10];
+       for(int i=0;i<5;i++)
+       {
+         cout<<"enter the number :"<<endl;
+	 cin>>arr[i];
+       }
+       for(int j=0;j<5;j++)
+       {
+	       cout<<"values:"<<arr[j]<<endl;
+       }
+
+       return 0;
+}
+    	 
---
+++ function.py
@@ -0,0 +1,11 @@
+#function
+
+def sum(self):
+    a=int(input("enter the number 1:"))
+    b=int(input("enter the number 2:"))
+
+    sum=a+b
+    print(f"sum of the number :{sum})
+
+sum()
+
