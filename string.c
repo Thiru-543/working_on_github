6c8192494df52a5de51da63b29741f07891b993c
thiru <sthiruselva11@gmail.com>
2026-03-24 12:54:54 +0530

    string

---
+++ string.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void)
+{
+	char name[20];
+	printf("enter the name :");
+	fgets(name,sizeof(name),stdin);
+	name[strcspn(name,"/n")]='/0;
+
+	printf("hello ! %s",name);
+	return 0;
+}	
+
